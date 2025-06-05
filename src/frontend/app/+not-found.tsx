2c62d2be6ecaf5788525c98896c9929f5706bdcc
Juan Segura <juan380@duck.com>
2025-06-05 23:29:25 +0200

    feat: enforce correct navigation from not found page

--- src/frontend/app/+not-found.tsx
+++ src/frontend/app/+not-found.tsx
@@ -1,32 +1,44 @@
 import { View, Text, StyleSheet } from 'react-native';
-import { Link, Stack, router } from 'expo-router';
+import { Link, Stack, useNavigation, useRouter } from 'expo-router';
 import { useTranslation } from 'react-i18next';
+import TitleSetter from '@/components/TitleSetter';
+import { useLayoutEffect } from 'react';
+import { HeaderBackButton } from '@react-navigation/elements';
+import { ActivityIndicator } from 'react-native-paper';
 
 export default function NotFoundScreen() {
   const { t } = useTranslation();
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => <HeaderBackButton onPress={() => router.navigate('/')} />,
+    });
+  }, [navigation]);
 
   return (
-    <>
+    <View style={styles.container}>
       <Stack.Screen
         options={{
           title: t('pages.not-found.title'),
         }}
       />
-      <View style={styles.container}>
-        <Text style={styles.title}>{t('pages.not-found.content.title')}</Text>
-        <Text style={styles.subtitle}>{t('pages.not-found.content.description')}</Text>
-        <Link
-          href='/'
-          style={styles.button}
-          onPress={(e) => {
-            e.preventDefault();
-            router.replace('/');
-          }}
-        >
-          {t('pages.not-found.content.back')}
-        </Link>
-      </View>
-    </>
+      <TitleSetter title={t('pages.not-found.title')} />
+
+      <Text style={styles.title}>{t('pages.not-found.content.title')}</Text>
+      <Text style={styles.subtitle}>{t('pages.not-found.content.description')}</Text>
+      <Link
+        href='/'
+        style={styles.button}
+        onPress={(e) => {
+          e.preventDefault();
+          router.replace('/');
+        }}
+      >
+        {t('pages.not-found.content.back')}
+      </Link>
+    </View>
   );
 }
 
